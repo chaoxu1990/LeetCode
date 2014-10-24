e167c760995dc6fce87d255f50d19e667b145ec1
chaoxu1990 <xc900411@gmail.com>
2014-10-23 21:29:11 -0700

    Improved ImplementstrStr().cpp

--- C++/ImplementstrStr().cpp
+++ C++/ImplementstrStr().cpp
@@ -7,39 +7,82 @@ needle is not part of haystack.
 
 */
 
-
+//solution 1: brute force
 class Solution {
 public:
     char *strStr(char *haystack, char *needle) {
-        char *p = haystack, *q = needle;
-        int cnt = 0;
-        while(*q != '\0')
+     int hLen = strlen(haystack), nLen = strlen(needle);
+
+     int i = 0, j = 0;
+
+     while(i<hLen&&j<nLen)
+     {
+         if(haystack[i] == needle[j])
+         {
+             i++;
+             j++;
+         }
+         else
+         {
+             i = i - j + 1;
+             j = 0;
+         }
+     }
+     if(j==nLen)
+        return haystack + i - j;
+    else
+        return nullptr;
+    }
+};
+
+
+//solution 2: KMP algorithm
+
+
+class Solution {
+public:
+    void GetNextval(char *p, int next[])
+    {
+        int i = -1, j = 0, len = strlen(p);
+        next[0] = i;
+
+        while(j<len-1)
         {
-            cnt++;
-            q++;
-        }
-        q = needle;
-        while(*p != '\0' && *q != '\0')
-        {
-            if(*p == *q)
+            if(i==-1 || p[i] == p[j])
             {
-                p++;
-                q++;
+                i++;
+                j++;
+                if(p[i]==p[j])
+                    next[j] = next[i];
+                else
+                    next[j] = i;
             }
             else
-            {
-                if(q == needle)
-                    p++;
-                else
-                {
-                    p = p - (q-needle) + 1;
-                }
-                q = needle;
-            }
+                i = next[i];
         }
-        if(*q == '\0')
-            return p-(q-needle);
-        else
-            return nullptr;
+
+    }
+    char *strStr(char *haystack, char *needle) {
+     int hLen = strlen(haystack), nLen = strlen(needle);
+     int i = 0, j = 0;
+     int *next = (int*)malloc(sizeof(int)*nLen);
+     GetNextval(needle, next);
+
+     while(i<hLen&&j<nLen)
+     {
+         if(j == -1 || haystack[i] == needle[j])
+         {
+             i++;
+             j++;
+         }
+         else
+         {
+             j = next[j];
+         }
+     }
+     if(j==nLen)
+        return haystack + i - j;
+    else
+        return nullptr;
     }
 };
