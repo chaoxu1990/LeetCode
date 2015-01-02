04918070c9bd260e9b5d3db3ba0f1628420f17c4
chaoxu1990 <xc900411@gmail.com>
2015-01-01 17:12:47 -0800

    Jan 1st

--- C++/Valid Palindrome.cpp
+++ C++/Valid Palindrome.cpp
@@ -54,15 +54,16 @@ public:
 
 class Solution {
 public:
-      bool isPalindrome(string s) {
-          transform(s.begin(), s.end(), s.begin(), ::tolower);
-          auto left = s.begin(), right = prev(s.end());
-          while (left < right) {
-              if (!::isalnum(*left))  ++left;
-              else if (!::isalnum(*right)) --right;
-              else if (*left != *right) return false;
-          }
-          return true;
-      }
+    bool isPalindrome(string s) {
+        for(int i = 0, j = s.size() - 1; i<j;i++,j--)
+        {
+            while((!isalnum(s[i])) && i<j)i++;
+            while(!isalnum(s[j]) && i<j)j--;   
+            if(tolower(s[i]) != tolower(s[j])) return false;
+        }      
+        return true;
+    }
 };
+
+
 
