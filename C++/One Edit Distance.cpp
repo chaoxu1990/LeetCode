577f34a32a8beb908fd74ad22e9f0f0ed87cfa86
Chao Xu <xc900411@gmail.com>
2015-01-20 22:15:58 -0800

    Update One Edit Distance.cpp

--- C++/One Edit Distance.cpp
+++ C++/One Edit Distance.cpp
@@ -28,3 +28,25 @@ public:
         return diffcount == 1;
     }
 };
+
+//上面那个解法虽然能过leetcode，但是j有可能会走到t[t.size()],虽然不会数组越界（是'\0')， 但是也不太好。
+//比如 s = "abc", t = "ab";
+
+class Solution {
+public:
+    bool isOneEditDistance(string s, string t) {
+        if(s.size() < t.size()) swap(s,t);
+        if(s.size() > t.size() + 1) return false;
+        int dist = 0;
+        for(int i = 0, j = 0; i<s.size() && j<t.size();i++, j++)
+        {
+            if(s[i] != t[j])
+            {
+                dist++;
+                if(s.size() != t.size())
+                j--;
+            }
+        }
+        return dist == 1 || (dist == 0 && s.size() == t.size()+1);
+    }
+}
