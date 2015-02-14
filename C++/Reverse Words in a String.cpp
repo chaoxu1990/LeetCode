dc898ff6c9bc5340239c05abcb0b4326c6c91b15
chaoxu1990 <xc900411@gmail.com>
2015-02-13 20:01:43 -0800

    Feb 13

--- C++/Reverse Words in a String.cpp
+++ C++/Reverse Words in a String.cpp
@@ -10,47 +10,32 @@ return "blue is sky the".
 
 class Solution {
 public:
-    void reverseString(string &s, const int begin, const int end)
+    void reverse(string &s, int start, int end)
     {
-        int i = begin, j = end-1;
-        while(i<j)
-        {
-            char tmp = s[i];
-            s[i++] = s[j];
-            s[j--] = tmp;
-        }
+        while(start < end)
+            swap(s[start++], s[end--]);
     }
     void reverseWords(string &s) {
-
-         int b = 0, e = s.size() - 1;
-
-        while(s[b] == ' ') b++;
-        while(s[e] == ' ') e--;
-
-        s.erase(s.begin(), s.begin() + b);
-        if(s.empty()) return;
-        s.erase(s.begin() + e - b + 1, s.end());
-
-        if(s.size() < 2) return;
-
-        for(int i = 0; i<s.size()-1; i++)
+        int start = 0, end = s.size() - 1;
+        while(start <= end)
         {
-            if(s[i] == ' ' && s[i+1] == ' ')
-            s.erase(s.begin()+i--);
+            if(s[start] == ' ') start++;
+            else if(s[end] == ' ') end--;
+            else break;
         }
-
-        reverseString(s, 0, s.size());
-
-        int begin = 0, end = 0, tmp = s.size() - 1;
-
-        for(size_t i = 0; i<s.size(); i++)
+        s = s.substr(start, end - start + 1);
+        
+        reverse(s, 0, s.size() - 1);
+        
+        for(int i = 0; i<s.size(); i++)
         {
-            if(s[i] == ' ')
-            {
-                reverseString(s, begin, i);
-                begin =  i + 1;
-            }
+            int j = s.find_first_of(' ', i);
+            if(j == -1) j = s.size();
+            reverse(s, i, j-1);
+            int k = s.find_first_not_of(' ', j);
+            if(k > j + 1)
+                s.erase(j+1, k-j-1);
+            i = j;
         }
-        reverseString(s, begin, s.size());
     }
 };
--- C++/Word Ladder II.cpp
+++ C++/Word Ladder II.cpp
@@ -87,3 +87,94 @@ public:
         }
     }
 };
+
+
+//Solution No.2
+//Using Bi-BFS, only take 131ms
+class Solution {
+public:
+    vector<vector<string> > result;
+    vector<string> tmp;
+    unordered_map<string, unordered_set<string> > table;
+
+    vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dict) {
+        tmp.clear();
+        result.clear();
+        table.clear();
+        unordered_map<string, bool> m1, m2;
+        queue<pair<string, int> > q1, q2;
+        bool found = false;
+        q1.push(pair<string, int>(start, 1));
+        q2.push(pair<string, int>(end,1));
+        tmp.push_back(start);
+        for(auto s: dict)
+        {
+            m1[s] = false;
+            m2[s] = false;
+        }
+        m1[start] = true;
+        m2[end] = true;
+
+        while(q1.empty() == false && q2.empty() == false)
+        {
+            if(q1.size() < q2.size())
+                found = BFS(q1, m1, m2, dict, table, true);
+            else
+                found = BFS(q2, m2, m1, dict, table, false);
+
+            if(found) {output(start, end); break;}
+        }
+
+        return result;
+    }
+
+     void output(string start, string end)
+    {
+        if(end == start)
+        {
+            result.push_back(tmp);
+            return;
+        }
+        for(auto it = table[start].begin(); it != table[start].end(); it++)
+        {
+            tmp.push_back(*it);
+            output(*it, end);
+            tmp.pop_back();
+        }
+    }
+    bool BFS(queue<pair<string, int> > &q, unordered_map<string, bool> &m, unordered_map<string, bool> &c, unordered_set<string> &dict, unordered_map<string, unordered_set<string> > &table, const bool flag)
+    {
+        bool found = false;
+        int level = q.front().second;
+        unordered_set<string> cur;
+        while(q.empty() == false && q.front().second == level)
+        {
+            string tmp = q.front().first;
+            string old = tmp;
+
+            for(int i = 0; i<old.size(); i++)
+            {
+                tmp = old;
+                for(int j = 0; j<26; j++)
+                {
+                    tmp[i] = j + 'a';
+                    if(tmp == old) continue;
+                    if(dict.find(tmp) != dict.end() && !m[tmp])
+                    {
+                        cur.insert(tmp);
+                        if(flag)
+                            table[old].insert(tmp);
+                        else
+                            table[tmp].insert(old);
+                        if(c[tmp] == true) found = true;
+                        q.push(pair<string, int>(tmp, level+1));
+                    }
+                }
+            }
+            q.pop();
+        }
+        for(auto it: cur)
+            m[it] = true;
+        return found;
+    }
+};
